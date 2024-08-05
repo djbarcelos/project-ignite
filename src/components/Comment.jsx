6c1a3a4080294725ae722995d0313f02f7cfc0f1
djbarcelos <djbarcelos@gmail.com>
2024-08-05 17:06:16 -0300

    Finalizado styled dos  comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -9,24 +9,26 @@ export function Comment() {
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
-                        <div className={styles.authorAndTime}></div>
-                        <strong>Diogo Jorge</strong>
-                        <time title="05 de Agosto as 14:56h" dateTime="2024-08-05 14:56:22">Cerca de 1h atrás</time>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diogo Jorge</strong>
+                            <time title="05 de Agosto as 14:56h" dateTime="2024-08-05 14:56:22">Cerca de 1h atrás</time>
+                        </div>
                         <button title='Remover comentário'>
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
 
                     <p>Muito bom Devon, parabéns!!</p>
                 </div>
-            </div>
 
-            <footer>
-                <button >
-                    <ThumbsUp size={20} />
-                    Aplaudir <span>20</span>
-                </button>
-            </footer>
+
+                <footer>
+                    <button >
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
         </div>
     )
 }
